160b834a22e8e4d31ea24f34f334525f3d562fcc
Monatric <seandumlao03@gmail.com>
2025-09-16 13:57:58 +0800

    feat: add states and styles for active btns

--- src/index.js
+++ src/index.js
@@ -2,4 +2,25 @@ import "./styles.css";
 import { Home } from "./modules/home";
 
 const content = document.querySelector("#content");
-content.appendChild(Home);
+const nav = document.querySelector("nav");
+let activeBtn = document.querySelector(".nav__button.active");
+
+nav.addEventListener("click", (event) => {
+  if (event.target.tagName === "BUTTON") {
+    activeBtn.classList.remove("active");
+    switch (event.target.textContent) {
+      case "Home":
+        content.appendChild(Home);
+        break;
+      case "Menu":
+        break;
+    }
+
+    activeBtn = event.target;
+    activeBtn.classList.add("active");
+  }
+});
+
+window.onload = function () {
+  content.appendChild(Home);
+};
