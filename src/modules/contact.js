c7b78f4fee1da9b3b124497c17d101435155bd63
Monatric <seandumlao03@gmail.com>
2025-09-16 17:23:49 +0800

    feat: build first half of contact module

---
+++ src/modules/contact.js
@@ -0,0 +1,64 @@
+import email from "../icons/email.svg";
+import location from "../icons/location.svg";
+import phone from "../icons/phone.svg";
+
+const contactSection = document.createElement("section");
+contactSection.classList.add("contact");
+
+const contactHeading = document.createElement("h1");
+contactHeading.classList.add("contact__heading");
+contactHeading.textContent = "Contact Us";
+
+const contactPara = document.createElement("p");
+contactPara.classList.add("contact__para");
+contactPara.textContent = `Visit us, call us, or make a reservation to experience authentic
+          Italian cuisine.`;
+
+const contactDetails = document.createElement("div");
+contactDetails.classList.add("contact__details");
+
+class ContactDetailsInfo {
+  contactDetailsInfo = document.createElement("div");
+  contactDetailsTitle = document.createElement("h3");
+  img = document.createElement("img");
+  contactDetailsDesc = document.createElement("p");
+
+  constructor(img, title, desc) {
+    this.img.src = img;
+    this.img.classList.add("svg");
+    this.contactDetailsTitle.textContent = title;
+    this.contactDetailsDesc.textContent = desc;
+    this.appendInfo();
+    this.addClasses();
+  }
+
+  appendToContainer() {
+    contactDetails.appendChild(this.contactDetailsInfo);
+  }
+
+  appendInfo() {
+    this.contactDetailsInfo.append(
+      this.contactDetailsTitle,
+      this.contactDetailsDesc
+    );
+    this.contactDetailsTitle.prepend(this.img);
+  }
+
+  addClasses() {
+    this.contactDetailsInfo.classList.add("contact__details_info");
+    this.contactDetailsTitle.classList.add("contact__details_title");
+    this.contactDetailsDesc.classList.add("contact__details_desc");
+  }
+}
+
+const contactDetailLocationTitle = "Location";
+const contactDetailLocationDesc = "123 Little Italy Street New York, NY 10013";
+new ContactDetailsInfo(
+  location,
+  contactDetailLocationTitle,
+  contactDetailLocationDesc
+).appendToContainer();
+
+contactSection.append(contactHeading, contactPara, contactDetails);
+
+export { contactSection as Contact };
