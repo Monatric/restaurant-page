c089cb923ebf85e50e039bf62fa18241c64e87dc
Monatric <seandumlao03@gmail.com>
2025-09-16 14:13:15 +0800

    feat: add menu module and create necessary elements

---
+++ src/modules/menu.js
@@ -0,0 +1,14 @@
+const menuSection = document.createElement("section");
+menuSection.classList.add("menu");
+
+const menuHeading = document.createElement("h1");
+menuHeading.classList.add("menu__heading");
+
+const menuPara = document.createElement("p");
+menuPara.classList.add("menu__para");
+
+const menuHeadingSpecialties = document.createElement("h2");
+menuHeadingSpecialties.classList.add("menu__heading", "specialties");
+
+const menuCardContainer = document.createElement("div");
+menuCardContainer.classList.add("menu__card_container");
